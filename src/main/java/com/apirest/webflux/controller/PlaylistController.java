5bb4d062d27019c68f39a99dce2daa0a20584b1e
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-12-12 21:24:03 -0300

    add event stream

--- src/main/java/com/apirest/webflux/controller/PlaylistController.java
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -2,9 +2,13 @@ package com.apirest.webflux.controller;
 
 import com.apirest.webflux.document.Playlist;
 import com.apirest.webflux.services.PlaylistService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("playlist")
@@ -30,4 +34,13 @@ public class PlaylistController {
 	public Mono<Playlist> save(@RequestBody final Playlist playlist) {
 		return service.save(playlist);
 	}
+
+	@GetMapping(value = "events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents()  {
+		final Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+		final Flux<Playlist> events = service.findAll();
+		System.out.println("Passou aqui events");
+		return Flux.zip(interval, events);
+	}
+
 }
